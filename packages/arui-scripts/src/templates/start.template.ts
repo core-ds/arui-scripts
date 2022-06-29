84e14623c452b8edde662c54974c1e4f168625e9
Никитин Дмитрий Александрович <DANikitin@alfabank.ru>
2022-06-29 16:54:13 +0300

    fix(start.template.ts): added condition for max_total_memory

--- packages/arui-scripts/src/templates/start.template.ts
+++ packages/arui-scripts/src/templates/start.template.ts
@@ -14,8 +14,12 @@ cat ./nginx.conf \\
     | sed 's/~~\\([a-zA-Z0-9_-]\\{1,\\}\\)~~/$\\1/g' \\
     > /etc/nginx/conf.d/default.conf
 
-# Достаем лимит памяти из cgroup, это то, как его докер задает.  https://shuheikagawa.com/blog/2017/05/27/memory-usage/
-max_total_memory=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+# Достаем лимит памяти из cgroup, это то, как его докер задает.
+if [[ -f /sys/fs/cgroup/cgroup.controllers ]]; then
+   max_total_memory=$(cat /sys/fs/cgroup/memory.max)
+else
+   max_total_memory=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+fi
 # Самой nodejs мы не можем отдать совсем всю память, операционная система+nginx все же требуют какого то количества.
 # Поэтому мы вычитаем 100мб на нужды ос
 node_memory_limit="$(($max_total_memory / 1024 / 1024 - 100))"
