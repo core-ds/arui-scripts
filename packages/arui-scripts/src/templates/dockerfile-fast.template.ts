8f255bd8fb9818a8498bddee71ef83ce4a7ba11b
Aleksandr Kitov <kitov.nn@gmail.com>
2022-12-06 14:31:42 +0100

    feat(docker-fast): don't build anything inside of container

--- packages/arui-scripts/src/templates/dockerfile-fast.template.ts
+++ packages/arui-scripts/src/templates/dockerfile-fast.template.ts
@@ -6,25 +6,23 @@ FROM ${configs.baseDockerImage}
 ARG START_SH_LOCATION
 ARG NGINX_CONF_LOCATION
 
+USER nginx
 WORKDIR /src
-ADD $START_SH_LOCATION /src/start.sh
-ADD $NGINX_CONF_LOCATION /src/nginx.conf
-ADD . /src
-RUN yarn install --no-progress --ignore-optional --frozen-lockfile --unsafe-perm && \\
-    yarn build && \\
-    yarn install --production --ignore-optional --frozen-lockfile --ignore-scripts --prefer-offline  && \\
-    yarn cache clean
-
-RUN chown -R nginx:nginx /src && chmod -R 755 /src && \
-       mkdir -p /var/lib/nginx && \
-       chown -R nginx:nginx /var/lib/nginx && \
-       chown -R nginx:nginx /var/log/nginx && \
-       chown -R nginx:nginx /etc/nginx/conf.d
+RUN mkdir -p /var/lib/nginx && \
+    chown -R nginx:nginx /var/lib/nginx && \
+    chown -R nginx:nginx /var/log/nginx && \
+    chown -R nginx:nginx /etc/nginx/conf.d
 
 RUN touch /var/run/nginx.pid && \
    chown -R nginx:nginx /var/run/nginx.pid
 
-USER nginx
+ADD $START_SH_LOCATION /src/start.sh
+ADD $NGINX_CONF_LOCATION /src/nginx.conf
+ADD --chown=nginx:nginx yarn.lock /src/yarn.lock
+ADD --chown=nginx:nginx package.json /src/package.json
+RUN yarn install --production --ignore-optional --frozen-lockfile --ignore-scripts --prefer-offline  && \\
+    yarn cache clean
+ADD --chown=nginx:nginx . /src
 `;
 
 export default applyOverrides('Dockerfile-fast', dockerfileTemplate);
