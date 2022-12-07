26144f9a91b94f5c1063fe6e7cfa9c962fca3dbc
Aleksandr Kitov <kitov.nn@gmail.com>
2022-12-07 09:25:04 +0100

    fix(commands): use correct naming for docker-build:compiled

--- packages/arui-scripts/src/bin/index.ts
+++ packages/arui-scripts/src/bin/index.ts
@@ -1,23 +1,23 @@
 #! /usr/bin/env node
 /* eslint import/no-dynamic-require: 0 */
 
-const commands = [
-    'start',
-    'build',
-    'docker-build',
-    'docker-build-compiled',
-    'test',
-    'ensure-yarn',
-    'archive-build',
-    'bundle-analyze',
-];
+const commands: Record<string, () => void> = {
+    start: () => require('../commands/start'),
+    build: () => require('../commands/build'),
+    'docker-build': () => require('../commands/docker-build'),
+    'docker-build:compiled': () => require('../commands/docker-build-compiled'),
+    test: () => require('../commands/test'),
+    'ensure-yarn': () => require('../commands/ensure-yarn'),
+    'archive-build': () => require('../commands/archive-build'),
+    'bundle-analyze': () => require('../commands/bundle-analyze'),
+};
 
-const command = process.argv[2];
+const command = process.argv[2] as string;
 
-if (!command || commands.indexOf(command) === -1) {
-    console.error(`Please specify one of available commands: ${commands.map(c => `"${c}"`).join(' ')}`);
+if (!command || !commands[command]) {
+    console.error(`Please specify one of available commands: ${Object.keys(commands).map(c => `"${c}"`).join(' ')}`);
 
     process.exit(-1);
 }
 
-require(`../commands/${command}`);
+commands[command]();
