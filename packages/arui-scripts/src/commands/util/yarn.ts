c2525358e7573e599bbdb20160453cc077f5a083
Aleksandr Kitov <kitov.nn@gmail.com>
2022-12-09 12:43:53 +0100

    fix(docker-build-compiled): add yarn2 required files before build

--- packages/arui-scripts/src/commands/util/yarn.ts
+++ packages/arui-scripts/src/commands/util/yarn.ts
@@ -1,9 +1,9 @@
 import configs from '../../configs/app-configs';
 import shell from 'shelljs';
 
-type Yarn = '1' | '2+' | 'unavailable';
+type YarnVersion = '1' | '2+' | 'unavailable';
 
-export function getYarnVersion(): Yarn {
+export function getYarnVersion(): YarnVersion {
     if (configs.useYarn && shell.which('yarn')) {
         const yarnVersion = shell.exec('yarn -v', { silent: true });
         const yarnMajorVersion = Number(yarnVersion.split('.')[0]);
@@ -26,6 +26,25 @@ export function getPruningCommand(): string {
         }
         case 'unavailable': {
             return 'npm prune --production';
+        }
+        default: {
+            return '';
+        }
+    }
+}
+
+export function getInstallProductionCommand(): string {
+    const yarnVersion = getYarnVersion();
+
+    switch (yarnVersion) {
+        case '1': {
+            return 'yarn install --production --ignore-optional --frozen-lockfile --ignore-scripts --prefer-offline';
+        }
+        case '2+': {
+            return 'yarn workspaces focus --production --all';
+        }
+        case 'unavailable': {
+            return 'npm install --production';
         }
         default: {
             return '';
