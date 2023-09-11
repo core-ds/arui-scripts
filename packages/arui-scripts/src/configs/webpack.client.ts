6ec2b42b574c08fc9432ade718ae7b9903155730
Alex Yatsenko <reme3d2y@gmail.com>
2023-09-11 11:56:22 +0300

    fix(webpack): restore postcss-discard-duplicates

--- packages/arui-scripts/src/configs/webpack.client.ts
+++ packages/arui-scripts/src/configs/webpack.client.ts
@@ -55,6 +55,78 @@ const clientAssetsPlugin = new AssetsPlugin({
     processOutput: processAssetsPluginOutput,
 });
 
+function getMinimizeConfig(mode: 'dev' | 'prod') {
+    if (mode === 'prod') {
+        return {
+            minimize: true,
+            minimizer: [
+                ...getImageMin(),
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            // we want terser to parse ecma 8 code. However, we don't want it
+                            // to apply any minfication steps that turns valid ecma 5 code
+                            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                            // sections only apply transformations that are ecma 5 safe
+                            // https://github.com/facebook/create-react-app/pull/4234
+                            ecma: 2017,
+                        },
+                        compress: {
+                            ecma: 5,
+                            // Disabled because of an issue with Uglify breaking seemingly valid code:
+                            // https://github.com/facebook/create-react-app/issues/2376
+                            // Pending further investigation:
+                            // https://github.com/mishoo/UglifyJS2/issues/2011
+                            comparisons: false,
+                            // Disabled because of an issue with Terser breaking valid code:
+                            // https://github.com/facebook/create-react-app/issues/5250
+                            // Pending futher investigation:
+                            // https://github.com/terser-js/terser/issues/120
+                            inline: 2,
+                        },
+                        mangle: {
+                            safari10: true,
+                        },
+                        output: {
+                            ecma: 5,
+                            comments: false,
+                            // Turned on because emoji and regex is not minified properly using default
+                            // https://github.com/facebook/create-react-app/issues/2488
+                            ascii_only: true,
+                        },
+                    },
+                    // Use multi-process parallel running to improve the build speed
+                    // Default number of concurrent runs: os.cpus().length - 1
+                    parallel: true,
+                }),
+                new CssMinimizerPlugin()
+            ].filter(Boolean)
+        }
+    }
+
+    if (configs.keepCssVars) {
+        // Каждый импорт css файла с переменными приводит к дублированию всего набора.
+        // Чтобы этого избежать — проходимся плагином, который удалит дубли из выходного файла.
+        return {
+            minimize: true,
+            minimizer: [
+                new CssMinimizerPlugin({
+                    minimizerOptions: {
+                        preset: () => ({
+                            plugins: [require('postcss-discard-duplicates')],
+                        }),
+                    },
+                }),
+            ]
+        };
+    }
+
+    return {
+        minimize: false,
+        minimizer: []
+    };
+}
+
 /**
  * Создает конфигурацию для сборки клиентского кода. Может быть использована для создания дополнительных конфигураций.
  * @param mode Режим сборки (dev или prod)
@@ -87,6 +159,7 @@ export const createSingleClientWebpackConfig = (mode: 'dev' | 'prod', entry: Ent
                 .replace(/\\/g, '/'),
     },
     optimization: {
+        ...getMinimizeConfig(mode),
         splitChunks: mode === 'prod'
             ? {
                 chunks: "all",
@@ -118,50 +191,6 @@ export const createSingleClientWebpackConfig = (mode: 'dev' | 'prod', entry: Ent
                 }
             }
             : false,
-        minimize: mode === 'prod',
-        minimizer: [
-            ...getImageMin(),
-            // This is only used in production mode
-            new TerserPlugin({
-                terserOptions: {
-                    parse: {
-                        // we want terser to parse ecma 8 code. However, we don't want it
-                        // to apply any minfication steps that turns valid ecma 5 code
-                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                        // sections only apply transformations that are ecma 5 safe
-                        // https://github.com/facebook/create-react-app/pull/4234
-                        ecma: 2017,
-                    },
-                    compress: {
-                        ecma: 5,
-                        // Disabled because of an issue with Uglify breaking seemingly valid code:
-                        // https://github.com/facebook/create-react-app/issues/2376
-                        // Pending further investigation:
-                        // https://github.com/mishoo/UglifyJS2/issues/2011
-                        comparisons: false,
-                        // Disabled because of an issue with Terser breaking valid code:
-                        // https://github.com/facebook/create-react-app/issues/5250
-                        // Pending futher investigation:
-                        // https://github.com/terser-js/terser/issues/120
-                        inline: 2,
-                    },
-                    mangle: {
-                        safari10: true,
-                    },
-                    output: {
-                        ecma: 5,
-                        comments: false,
-                        // Turned on because emoji and regex is not minified properly using default
-                        // https://github.com/facebook/create-react-app/issues/2488
-                        ascii_only: true,
-                    },
-                },
-                // Use multi-process parallel running to improve the build speed
-                // Default number of concurrent runs: os.cpus().length - 1
-                parallel: true,
-            }),
-            new CssMinimizerPlugin()
-        ].filter(Boolean),
         nodeEnv: mode === 'prod' ? 'production' : false,
 
         // Оптимизации времени билда, см https://webpack.js.org/guides/build-performance/
