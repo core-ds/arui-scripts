f8960f09358df76daefa564ec7dd845a6a722e2a
Aleksandr Kitov <kitov.nn@gmail.com>
2023-12-12 10:58:53 +0300

    fix(modules): correctly add css prefixes for nested at-rules

--- packages/arui-scripts/src/plugins/postcss-prefix-selector.ts
+++ packages/arui-scripts/src/plugins/postcss-prefix-selector.ts
@@ -1,6 +1,6 @@
 // TODO: remove eslint-disable
 /* eslint-disable no-param-reassign */
-import { PluginCreator } from 'postcss';
+import { PluginCreator, Root } from 'postcss';
 
 type PostCssPrefixOptions = {
     prefix: string;
@@ -8,24 +8,47 @@ type PostCssPrefixOptions = {
 
 const VisitedRule = Symbol('VisitedRule');
 
+type RootWithVisitedRule = Root & {
+    [VisitedRule]?: boolean;
+};
+
+type NodeWithVisitedAndParent = {
+    [VisitedRule]?: boolean;
+    parent?: NodeWithVisitedAndParent;
+};
+
+const KEYFRAME_RULES = [
+    'keyframes',
+    '-webkit-keyframes',
+    '-moz-keyframes',
+    '-o-keyframes',
+];
+
 const postCssPrefix: PluginCreator<PostCssPrefixOptions> = (options) => {
     const prefix = options?.prefix || '.prefix ';
 
     return {
         postcssPlugin: '@alfalab/postcss-prefix-selector',
-        RootExit(root) {
-            if ((root as any)[VisitedRule]) {
+        RootExit(root: RootWithVisitedRule) {
+            if (root[VisitedRule]) {
                 return;
             }
 
-            (root as any)[VisitedRule] = true;
+            root[VisitedRule] = true;
 
             root.walkRules((rule) => {
-                if (
-                    !rule.parent ||
-                    (rule.parent.type !== 'root' &&
-                        (rule.parent.type !== 'atrule' || (rule.parent as any).name !== 'media'))
-                ) {
+                const hasParent = !!rule.parent;
+                const parentIsAtRule = rule.parent && rule.parent.type === 'atrule';
+                const parentIsKeyframe = parentIsAtRule && KEYFRAME_RULES.includes((rule.parent as any).name);
+
+                if (hasParent && parentIsKeyframe) {
+                    // Нам не нужно добавлять префиксы для keyframes и элементов внутри них
+                    return;
+                }
+
+                (rule as NodeWithVisitedAndParent)[VisitedRule] = true;
+                if (isAnyParentVisited(rule)) {
+                    // если хотя бы один из родителей (кроме root) был посещен, то не добавляем префикс
                     return;
                 }
                 rule.selectors = rule.selectors.map((selector, index) => {
@@ -39,6 +62,26 @@ const postCssPrefix: PluginCreator<PostCssPrefixOptions> = (options) => {
         },
     };
 };
+
+/**
+ * Проверяет, что хотя бы один из родителей был посещен. Если посещен Root элемент, то возвращает false
+ * @param rule
+ */
+function isAnyParentVisited(rule: NodeWithVisitedAndParent) {
+    let { parent } = rule;
+
+    while (parent) {
+        if (!parent.parent)  {
+            return false;
+        }
+        if (parent[VisitedRule]) {
+            return true;
+        }
+        parent = parent.parent;
+    }
+
+    return false;
+}
 
 postCssPrefix.postcss = true;
 
