f97f2ba9fd22473e2a3725dfc3699c6b0577ed15
Aleksandr Kitov <kitov.nn@gmail.com>
2025-01-24 12:44:46 +0300

    fix(wmf): disable split chunk for remoteEntry chunk

---
+++ packages/arui-scripts/src/plugins/turn-off-split-remote-entry.ts
@@ -0,0 +1,90 @@
+import { types } from 'util';
+
+import { Compiler, OptimizationSplitChunksCacheGroup,RspackPluginInstance } from '@rspack/core';
+
+/**
+ * Force remote entry not be affected by user's chunkSplit strategy,
+ * Otherwise, the remote chunk will not be loaded correctly.
+ * @see https://github.com/web-infra-dev/rsbuild/discussions/1461#discussioncomment-8329790
+ */
+export class TurnOffSplitRemoteEntry implements RspackPluginInstance {
+    name: string;
+
+    constructor(wmfContainerName: string) {
+        this.name = wmfContainerName;
+    }
+
+    apply(compiler: Compiler): void {
+        const { splitChunks } = compiler.options.optimization;
+
+        if (!splitChunks) {
+            return;
+        }
+
+        /* eslint-disable no-param-reassign */
+        const applyPatch = (cacheGroup: OptimizationSplitChunksCacheGroup | false) => {
+            if (typeof cacheGroup !== 'object' || types.isRegExp(cacheGroup)) {
+                return;
+            }
+
+            // cacheGroup.chunks will inherit splitChunks.chunks
+            // so we only need to modify the chunks that are set separately.
+            const { chunks } = cacheGroup;
+
+            if (!chunks || chunks === 'async') {
+                return;
+            }
+
+            if (typeof chunks === 'function') {
+                const prevChunks = chunks;
+
+                cacheGroup.chunks = (chunk) => {
+                    if (chunk.name && chunk.name === this.name) {
+                        return false;
+                    }
+
+                    return prevChunks(chunk);
+                };
+
+                return;
+            }
+
+            if (chunks === 'all') {
+                cacheGroup.chunks = (chunk) => {
+                    if (chunk.name && chunk.name === this.name) {
+                        return false;
+                    }
+
+                    return true;
+                };
+
+                return;
+            }
+
+            if (chunks === 'initial') {
+                cacheGroup.chunks = (chunk) => {
+                    if (chunk.name && chunk.name === this.name) {
+                        return false;
+                    }
+
+                    return chunk.isOnlyInitial();
+                };
+            }
+        };
+        /* eslint-enable no-param-reassign */
+
+        // patch splitChunk.chunks
+        applyPatch(splitChunks);
+
+        const { cacheGroups } = splitChunks;
+
+        if (!cacheGroups) {
+            return;
+        }
+
+        // patch splitChunk.cacheGroups[key].chunks
+        for (const cacheGroupKey of Object.keys(cacheGroups)) {
+            applyPatch(cacheGroups[cacheGroupKey]);
+        }
+    }
+}
