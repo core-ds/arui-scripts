db28c1d7dc689831394fed2f8ff9ce4feafb23db
Aleksandr Kitov <kitov.nn@gmail.com>
2025-04-09 17:57:51 +0300

    chore(arui-scripts): pass options to brotli compression in a correct way

--- packages/arui-scripts/src/configs/webpack.client.ts
+++ packages/arui-scripts/src/configs/webpack.client.ts
@@ -2,6 +2,7 @@
 /* eslint-disable global-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import path from 'path';
+import * as zlib from 'zlib';
 
 import getCSSModuleLocalIdent from 'react-dev-utils/getCSSModuleLocalIdent';
 import ReactRefreshTypeScript from 'react-refresh-typescript';
@@ -408,12 +409,14 @@ export const createSingleClientWebpackConfig = (
             }),
         mode === 'prod' &&
             checkNodeVersion(10) &&
-            new CompressionPlugin({
+            new CompressionPlugin<zlib.BrotliOptions>({
                 filename: '[file].br',
                 algorithm: 'brotliCompress',
                 test: /\.(js|css|html|svg)$/,
                 compressionOptions: {
-                    level: 11,
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+                    },
                 },
                 threshold: 10240,
                 minRatio: 0.8,
