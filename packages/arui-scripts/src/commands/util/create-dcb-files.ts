6da9fd775ecc92a6c4f862822cb19fafcf8dfbeb
Aleksandr Kitov <kitov.nn@gmail.com>
2025-04-16 17:18:52 +0300

    feat(arui-scripts): create dcb files on build

---
+++ packages/arui-scripts/src/commands/util/create-dcb-files.ts
@@ -0,0 +1,28 @@
+import { createDcbFile,findFilesToCompress } from '@alfalab/brotli-dcb-builder';
+
+import { configs } from '../../configs/app-configs';
+
+export async function createDcbFiles() {
+    if (!configs.previousVersionPath) {
+        return;
+    }
+
+    const previousVersions = Array.isArray(configs.previousVersionPath)
+        ? configs.previousVersionPath
+        : [configs.previousVersionPath];
+
+    const filesToCompressPromise = previousVersions.map((dictionaryDir) => findFilesToCompress({
+        baseDir: configs.clientOutputPath,
+        outDir: configs.clientOutputPath,
+        allowedExtensions: ['js', 'css'],
+        dictionaryDir,
+    }));
+
+    const filesToCompress = (await Promise.all(filesToCompressPromise)).flat();
+
+    const promises = filesToCompress.map((fileSettings) => createDcbFile(fileSettings));
+
+    await Promise.all(promises);
+
+    console.log(`Created ${filesToCompress.length} dcb archives`);
+}
