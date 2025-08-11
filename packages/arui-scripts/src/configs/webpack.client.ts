bf3072e164d19179c05523900036a9841497cdcb
Aleksandr Kitov <kitov.nn@gmail.com>
2025-08-11 17:35:07 +0300

    feat(terser): use default terser options

--- packages/arui-scripts/src/configs/webpack.client.ts
+++ packages/arui-scripts/src/configs/webpack.client.ts
@@ -67,44 +67,7 @@ function getMinimizeConfig(mode: 'dev' | 'prod') {
         return {
             minimize: true,
             minimizer: [
-                new TerserPlugin({
-                    terserOptions: {
-                        parse: {
-                            // we want terser to parse ecma 8 code. However, we don't want it
-                            // to apply any minfication steps that turns valid ecma 5 code
-                            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                            // sections only apply transformations that are ecma 5 safe
-                            // https://github.com/facebook/create-react-app/pull/4234
-                            ecma: 2017,
-                        },
-                        compress: {
-                            ecma: 5,
-                            // Disabled because of an issue with Uglify breaking seemingly valid code:
-                            // https://github.com/facebook/create-react-app/issues/2376
-                            // Pending further investigation:
-                            // https://github.com/mishoo/UglifyJS2/issues/2011
-                            comparisons: false,
-                            // Disabled because of an issue with Terser breaking valid code:
-                            // https://github.com/facebook/create-react-app/issues/5250
-                            // Pending futher investigation:
-                            // https://github.com/terser-js/terser/issues/120
-                            inline: 2,
-                        },
-                        mangle: {
-                            safari10: true,
-                        },
-                        output: {
-                            ecma: 5,
-                            comments: false,
-                            // Turned on because emoji and regex is not minified properly using default
-                            // https://github.com/facebook/create-react-app/issues/2488
-                            ascii_only: true,
-                        },
-                    },
-                    // Use multi-process parallel running to improve the build speed
-                    // Default number of concurrent runs: os.cpus().length - 1
-                    parallel: true,
-                }),
+                new TerserPlugin(),
                 new CssMinimizerPlugin(),
             ].filter(Boolean),
         };
