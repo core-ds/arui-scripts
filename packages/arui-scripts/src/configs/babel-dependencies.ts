adfad8a92ba0bb089f442b66e579b2dc3bc0a3c8
Боровский Дмитрий <DBorovskiy@alfabank.ru>
2026-06-30 02:18:27 +0300

    chore(deps): unpin @babel/core, sync babel-jest@29, bump core-js@3.49

--- packages/arui-scripts/src/configs/babel-dependencies.ts
+++ packages/arui-scripts/src/configs/babel-dependencies.ts
@@ -10,8 +10,7 @@ export const babelDependencies = applyOverrides('babelDependencies', {
                 // Allow importing core-js in entrypoint and use browserlist to select polyfills
                 useBuiltIns: 'entry',
                 // Set the corejs version we are using to avoid warnings in console
-                // This will need to change once we upgrade to corejs@3
-                corejs: '3.32',
+                corejs: '3.49',
                 // Exclude transforms that make all code slower
                 exclude: ['transform-typeof-symbol'],
             },
