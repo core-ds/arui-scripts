b2948cf1a1d24022d976d2948e2e6c671209fadb
Боровский Дмитрий <DBorovskiy@alfabank.ru>
2026-06-30 02:22:35 +0300

    style(commands): sort imports in start/start-prod server

--- packages/arui-scripts/src/commands/start/server.ts
+++ packages/arui-scripts/src/commands/start/server.ts
@@ -1,5 +1,5 @@
-import { supportingNode } from '../../configs/supporting-node';
 import { webpackServerConfig } from '../../configs/rspack.server.dev';
+import { supportingNode } from '../../configs/supporting-node';
 import { runServerWatchCompiler } from '../util/run-server-watch-compiler';
 
 process.env.BROWSERSLIST = supportingNode.join(',');
