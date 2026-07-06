9227795c08eda5cba4bb60f494ac9d789e032522
Aleksandr Kitov <kitov.nn@gmail.com>
2026-07-06 15:22:25 +0300

    docs(*): add SSR modules examples

--- packages/example/src/module-mounters/suspense-mounter.tsx
+++ packages/example/src/module-mounters/suspense-mounter.tsx
@@ -1,29 +1,33 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 
 import { Spinner } from '@alfalab/core-components/spinner';
 import { Underlay } from '@alfalab/core-components/underlay';
-import {
-    createLazyMounter,
-    createModuleLoader,
-    createServerStateModuleFetcher,
-    type MountableModule,
-} from '@alfalab/scripts-modules';
+import { createServerStateModuleFetcher, type BaseModuleState } from '@alfalab/scripts-modules';
+import { createSsrMounter } from '@alfalab/scripts-modules/ssr';
 
 type ModuleRunParams = {
     name: string;
     counter: number;
 };
 
-const loader = createModuleLoader<MountableModule<ModuleRunParams>>({
-    hostAppId: 'example',
-    moduleId: 'ServerStateModule',
-    getModuleResources: createServerStateModuleFetcher({ baseUrl: 'http://localhost:8082' }),
+const getModuleResources = createServerStateModuleFetcher({
+    baseUrl: 'http://localhost:8082',
 });
 
-const LazyModule = lazy(createLazyMounter({ loader }));
+const { ModuleComponent: ServerStateSsrModule } = createSsrMounter<
+    ModuleRunParams,
+    ModuleRunParams,
+    undefined,
+    BaseModuleState
+>({
+    hostAppId: 'example',
+    moduleId: 'ServerStateModule',
+    getModuleResources,
+});
 
 export const SuspenseMounter = () => {
     const [counter, setCounter] = React.useState(1);
+    const runParams = { name: 'Vasia', counter };
 
     return (
         <Underlay
@@ -39,7 +43,11 @@ export const SuspenseMounter = () => {
                 </button>
 
                 <Suspense fallback={<Spinner />}>
-                    <LazyModule name='Vasia' counter={counter} />
+                    <ServerStateSsrModule
+                        instanceId='server-state-ssr-demo'
+                        runParams={runParams}
+                        ssrRunParams={runParams}
+                    />
                 </Suspense>
             </div>
         </Underlay>
